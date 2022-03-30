927e846dca5e612ec0df1ed4a85c814760435a03
ksennn <kseniakabaeva@mail.ru>
2022-03-30 19:50:57 +0300

    Create seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+Post.destroy_all
+puts "Destroyed everything you touch"
+
+# posts = [
+# {
+# 	name: "New Album",
+# 	title: "The Bad Seeds",
+# 	author: "Nick Cave",
+#   image: File.open(Rails.root.join('public', 'images', 'sheep.png')),
+# 	content: "Tracklist"
+# },
+# {
+# 	name: "Old Album",
+# 	title: "The Bad Seeds Old",
+# 	author: "Nick Cave Old Too",
+#   image: File.open(Rails.root.join('public', 'images', 'sheep.png')),
+# 	content: "Some Old Tracklist"
+# }
+# ]
+#
+# posts.each do |post|
+# 	postik = Post.create(post)
+# 	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
+# end
