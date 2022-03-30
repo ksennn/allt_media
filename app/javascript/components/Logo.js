29b2da309fa989ded81c6aa7815d68cfd25f83ab
ksennn <kseniakabaeva@mail.ru>
2022-03-30 19:47:16 +0300

    Create Logo.js

---
+++ app/javascript/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+import 'stylesheets/A_logo.scss'
+import Logo from "images/logo.svg"
+
+return(
+      <img src={Logo} className = {`logo ${checkLogoStyle}`}>
+      </img>
+  )
+}
+
+
+export default Logo
